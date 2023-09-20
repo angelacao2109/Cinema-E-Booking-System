7b4cda190635f7188cced48b1a7c8e1fccdd7fdf
brandonmoua24 <124404056+brandonmoua24@users.noreply.github.com>
2023-09-20 17:05:05 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home/Home";
+
+import Registration from "./Components/Registration/Registration";
+import Profile from "./Components/Profile/Profile";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
