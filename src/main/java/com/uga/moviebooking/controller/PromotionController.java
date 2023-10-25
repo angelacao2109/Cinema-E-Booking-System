930fdebc1031f6bbc41c52040ef48e7f2048f238
Iyanna Yapo <Iyannayapo123@gmail.com>
2023-10-24 22:13:36 -0400

    I forgot about auto wired :(

--- src/main/java/com/uga/moviebooking/controller/PromotionController.java
+++ src/main/java/com/uga/moviebooking/controller/PromotionController.java
@@ -5,7 +5,10 @@ import com.uga.moviebooking.model.dto.PromotionDto;
 import com.uga.moviebooking.model.movie.MovieService;
 import com.uga.moviebooking.model.promotion.Promotion;
 import com.uga.moviebooking.model.promotion.PromotionService;
+import com.uga.moviebooking.model.user.User;
+import com.uga.moviebooking.model.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +22,11 @@ import java.util.Optional;
 public class PromotionController {
 
     PromotionService promotionService;
+    @Autowired
+    public PromotionController(PromotionService promotionService) {
+        this.promotionService = promotionService;
+    }
+    UserController userController;
 
     @CrossOrigin(origins = "http://localhost:5173")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
@@ -32,9 +40,9 @@ public class PromotionController {
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/delete")
     public ResponseEntity<String> deletePromotion(@RequestBody PromotionDto promo) {
-       if( promotionService.removePromotion(promo.getId())){
-           return ResponseEntity.ok("Promotion successfully removed");
-       }
+        if (promotionService.removePromotion(promo.getId())) {
+            return ResponseEntity.ok("Promotion successfully removed");
+        }
         return ResponseEntity.ok("Promotion could not be removed or does not exist.");
     }
 
@@ -42,7 +50,7 @@ public class PromotionController {
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/update")
     public ResponseEntity<String> updatePromotion(@RequestBody PromotionDto promo) {
-        if( promotionService.updatePromotion(promo.getId(),promo)){
+        if (promotionService.updatePromotion(promo.getId(), promo)) {
             return ResponseEntity.ok("Promotion successfully updated");
         }
         return ResponseEntity.ok("Promotion could not be updated or does not exist.");
@@ -51,25 +59,46 @@ public class PromotionController {
     @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/getpromotion")
     public ResponseEntity<Promotion> getPromotion(@RequestBody long ID) {
-        Promotion promotion =  promotionService.retrievePromotion(ID);
-        if(promotion != null){
+        Promotion promotion = promotionService.retrievePromotion(ID);
+        if (promotion != null) {
             return ResponseEntity.ok(promotion);
-        }else{
+        } else {
             return ResponseEntity.notFound().build();
         }
 
     }
+
     //may need to check this method
     @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/getActivePromotions")
-    public ResponseEntity<List<Promotion>> activePromotions(){
+    public ResponseEntity<List<Promotion>> activePromotions() {
         Date day = new Date(System.currentTimeMillis());
         List<Promotion> list = promotionService.findActivePromotions(day);
-        if(list == null || list.isEmpty()) {
+        if (list == null || list.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(list);
 
     }
 
+    //Send promo via email endpoint
+    @CrossOrigin(origins = "http://localhost:5173")
+    //@PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/send-promotion-email")
+    public ResponseEntity<String> sendPromotionEmail(@RequestBody long ID) {
+        // Validate admin permissions.
+
+        Promotion promotion = promotionService.retrievePromotion(ID);
+        if (promotion != null) {
+            List<User> userEmails = userController.getUser("user");
+            promotionService.sendPromotionEmail(promotion, userEmails);
+            return ResponseEntity.ok("Promotion emails sent successfully.");
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Permission denied.");
+        }
+
+    }
 }
+
+
+
