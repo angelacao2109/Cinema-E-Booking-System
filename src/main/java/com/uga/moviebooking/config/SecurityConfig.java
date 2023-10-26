c79475a1d03364bc465d3d02f7b3d91b1eaa7f74
angelacao2109 <angelacao2109@yahoo.com>
2023-10-26 12:14:59 -0400

    SpringSecurity updates

--- src/main/java/com/uga/moviebooking/config/SecurityConfig.java
+++ src/main/java/com/uga/moviebooking/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.uga.moviebooking.config;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
@@ -18,9 +19,16 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -61,6 +69,22 @@ public class SecurityConfig {
 //        source.registerCorsConfiguration("/**", configuration);
 //        return source;
 //    }
+
+    @Component
+    public class CorsFilter extends OncePerRequestFilter {
+
+        @Override
+        protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response,
+                                        final FilterChain filterChain) throws ServletException, IOException {
+            response.addHeader("Access-Control-Allow-Origin", "*");
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD");
+            response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+            response.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials");
+            response.addHeader("Access-Control-Allow-Credentials", "true");
+            response.addIntHeader("Access-Control-Max-Age", 10);
+            filterChain.doFilter(request, response);
+        }
+    }
 
     @Bean
     public static PasswordEncoder passwordEncoder(){
