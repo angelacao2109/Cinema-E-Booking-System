72bd6e3873a3ea7e7b202fef481a953148ebffb3
Aidan LeCroy <lecroyaidan@gmail.com>
2023-11-29 19:11:05 -0500

    Added ticket price endpoint at GET /api/booking/price

--- src/main/java/com/uga/moviebooking/controller/TheatreController.java
+++ src/main/java/com/uga/moviebooking/controller/TheatreController.java
@@ -1,10 +1,11 @@
 package com.uga.moviebooking.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/theatre")
 public class TheatreController {
+
+
 }
