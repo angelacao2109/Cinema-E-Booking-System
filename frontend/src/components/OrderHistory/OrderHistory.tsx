a47f1bb35ce400825d73f71f61fe0dc3df63e235
angelacao2109 <angelacao2109@yahoo.com>
2023-12-04 17:10:24 -0500

    starting on order history not done

--- frontend/src/components/OrderHistory/OrderHistory.tsx
+++ frontend/src/components/OrderHistory/OrderHistory.tsx
@@ -1,33 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./OrderHistory.css";
 
-// TODO AUTH
+
+interface Ticket {
+  type: string;
+  seatRow: string;
+  seatCol: number;
+}
+
+interface Movie {
+  title: string;
+  
+}
+
+interface Showtime {
+  date: string;
+  time: string;
+
+}
+
+interface Order {
+  movie: Movie;
+  showtime: Showtime;
+  tickets: Ticket[];
+  totalCost: number;
+}
 
 const OrderHistory: React.FC = () => {
-  const orders = [
-    {
-      movieName: "Movie A",
-      seats: ["A1", "A2"],
-      date: "10-01-2023",
-      time: "15:00",
-      tickets: [
-        { type: "Adult", quantity: 1, cost: "$15" },
-        { type: "Child", quantity: 1, cost: "$10" },
-      ],
-      total: "$25",
-    },
-    {
-      movieName: "Movie B",
-      seats: ["B1", "B2", "B3"],
-      date: "09-12-2023",
-      time: "18:00",
-      tickets: [
-        { type: "Adult", quantity: 2, cost: "$30" },
-        { type: "Senior", quantity: 1, cost: "$10" },
-      ],
-      total: "$40",
-    },
-  ];
+  const [orders, setOrders] = useState<Order[]>([]);
+
+ 
+  const authToken = /* retrieve auth token method */;
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const response = await axios.get<{ history: Order[] }>('http://localhost:8080/api/orders', {
+          headers: { Authorization: authToken }
+        });
+        setOrders(response.data.history);
+      } catch (error) {
+        console.error('Error fetching order history:', error);
+      }
+    };
+
+    fetchOrders();
+  }, []);
 
   return (
     <div className="summary-container">
@@ -35,19 +54,19 @@ const OrderHistory: React.FC = () => {
 
       {orders.map((order, index) => (
         <div key={index} className="order-item">
-          <h2>{order.movieName}</h2>
+          <h2>{order.movie.title}</h2>
           <h3>
-            Date and Time: {order.date} at {order.time}
+            Date and Time: {order.showtime.date} at {order.showtime.time}
           </h3>
-          <p>Seats: {order.seats.join(", ")}</p>
+          <p>Seats: {order.tickets.map(t => `${t.seatRow}${t.seatCol}`).join(", ")}</p>
           <ul>
             {order.tickets.map((ticket, tIndex) => (
               <li key={tIndex}>
-                {ticket.type}: {ticket.quantity} - {ticket.cost}
+                {ticket.type}: {/* Quantity and cost information here */}
               </li>
             ))}
           </ul>
-          <p>Total: {order.total}</p>
+          <p>Total: ${(order.totalCost / 100).toFixed(2)}</p>
           <hr />
         </div>
       ))}
